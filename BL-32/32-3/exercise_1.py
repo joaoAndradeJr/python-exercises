8277d392da16539dd75ee487137116a4b6a5b3df
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-12-01 21:48:03 -0400

    feat: exercise 1

---
+++ BL-32/32-3/exercise_1.py
@@ -0,0 +1,26 @@
+"""
+Escreva um programa que retorne uma lista com os valores numéricos de 1 a N,
+mas com as seguintes exceções :
+Números divisíveis por 3 deve aparecer como 'Fizz' ao invés do número;
+Números divisíveis por 5 devem aparecer como 'Buzz' ao invés do número;
+Números divisíveis por 3 e 5 devem aparecer como 'FizzBuzz' ao invés do número'
+Ex: 3 -> [1, 2, "Fizz"] .
+"""
+
+
+def fizzBuzz(number):
+    list_numbers = []
+    counter = 1
+
+    while (counter <= number):
+        if (counter % 3 == 0 and counter % 5 == 0):
+            list_numbers.append("FizzBuzz")
+        elif (counter % 5 == 0):
+            list_numbers.append("Buzz")
+        elif (counter % 3 == 0):
+            list_numbers.append("Fizz")
+        else:
+            list_numbers.append(counter)
+        counter += 1
+
+    return list_numbers
