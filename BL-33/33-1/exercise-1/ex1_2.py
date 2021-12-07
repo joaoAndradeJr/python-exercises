0eebb9b15c0622d55175b6ba55c7fe9ef335f470
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-12-07 17:07:25 -0400

    feat: exercise 2

---
+++ BL-33/33-1/exercise-1/ex1_2.py
@@ -0,0 +1,27 @@
+"""
+Para exercitar nossa capacidade de abstração, vamos modelar algumas partes
+de um software de geometria. Como poderíamos modelar um objeto retângulo?
+🐦 Para ajudar, segue o exemplo do quadrado. Vamos utilizar a seguinte
+notação para descrever nossas abstrações:
+
+Nome da abstração
+Quadrado
+
+atributos/estados
+- lado (tamanho)
+
+comportamentos
+- calcular area (lado * lado)
+- calcular perímetro (4 * lado)
+"""
+
+"""
+Nome da abstração - Retangulo
+
+atributos
+- altura
+- base
+
+comportamentos
+- calcular area = (base * altura) 
+"""
